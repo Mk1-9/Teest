8a18396ee652857d82f97839c7aa41bd4841643a
Mikail <mikailyavuz751900@gmail.com>
2020-05-06 11:08:09 +0200

    Initial Commit

---
+++ Teest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Teest
+//
+//  Created by Reyhan 2 on 06/05/2020.
+//  Copyright © 2020 Mikail. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
